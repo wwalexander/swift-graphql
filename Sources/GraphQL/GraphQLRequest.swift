419471e418412e8438d7b7c30a0506653c0a6ad4
William Alexander <6404469+wwalexander@users.noreply.github.com>
2025-10-07 23:18:45 +0200

    Add GraphQLRequest

---
+++ Sources/GraphQL/GraphQLRequest.swift
@@ -0,0 +1,24 @@
+public struct GraphQLRequest<Variables> {
+    public var query: String
+    public var variables: Variables?
+    
+    init(query: String, variables: Variables?) {
+        self.query = query
+        self.variables = variables
+    }
+    
+    public init(query: String) where Variables == GraphQLRequestVariablesNone {
+        self.query = query
+    }
+    
+    public init(query: String, variables: Variables) {
+        self.query = query
+        self.variables = variables
+    }
+    
+}
+
+public enum GraphQLRequestVariablesNone {}
+
+extension GraphQLRequest: Decodable where Variables: Decodable {}
+extension GraphQLRequest: Encodable where Variables: Encodable {}
