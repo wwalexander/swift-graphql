21a4a0fbbb6ad0c1de98f6125323cb50b8991f55
William Alexander <6404469+wwalexander@users.noreply.github.com>
2025-10-08 01:21:50 +0200

    Add headers to GraphQLHTTPClient

--- Sources/GraphQLHTTP/GraphQLHTTPClient.swift
+++ Sources/GraphQLHTTP/GraphQLHTTPClient.swift
@@ -3,15 +3,18 @@ import Foundation
 
 public struct GraphQLHTTPClient {
     public let endpoint: URL
-    public let urlSession: URLSession
+    let headers: [String: String?]
+    let urlSession: URLSession
     let jsonDecoder: JSONDecoder
     let jsonEncoder: JSONEncoder
     
     public init(
         endpoint: URL,
+        headers: [String: String?] = [:],
         urlSession: URLSession = .shared
     ) {
         self.endpoint = endpoint
+        self.headers = headers
         self.urlSession = urlSession
         self.jsonDecoder = .init()
         self.jsonEncoder = .init()
@@ -22,6 +25,11 @@ public struct GraphQLHTTPClient {
         urlRequest.httpMethod = "POST"
         urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         urlRequest.setValue("application/graphql-response+json, application/json", forHTTPHeaderField: "Accept")
+        
+        for header in headers {
+            urlRequest.setValue(header.value, forHTTPHeaderField: header.key)
+        }
+        
         urlRequest.httpBody = try jsonEncoder.encode(request)
         let (data, _) = try await urlSession.data(for: urlRequest)
         return try jsonDecoder.decode(GraphQLResponse<Data>.self, from: data).result.get()
