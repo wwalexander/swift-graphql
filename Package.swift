c5208ddf7799ab0d7c2fba245f1ef79c8124f144
William Alexander <6404469+wwalexander@users.noreply.github.com>
2025-10-07 22:57:10 +0200

    Remove comments from Package.swift

--- Package.swift
+++ Package.swift
@@ -5,15 +5,12 @@ import PackageDescription
 let package = Package(
     name: "swift-graphql",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GraphQL",
             targets: ["GraphQL"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GraphQL"
         ),
